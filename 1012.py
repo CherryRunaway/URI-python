babc9aa5ca249b339ed535fef544a84ff6852a5c
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-11 21:43:44 -0300

    Create 1012.py

---
+++ 1012.py
@@ -0,0 +1,14 @@
+def main():
+    linha1 = input().split(" ")
+    pi = 3.14159
+    a, b, c = linha1
+    
+    triangulo = (float(a) * float(c))/2
+    circulo = pi * (float(c)* float(c))
+    trapezio = float(c) *(float(a) + float(b)) / 2
+    quadrado = float(b) * float(b)
+    retangulo = float(a) * float(b)
+    
+    
+    print("TRIANGULO: %0.3f\nCIRCULO: %0.3f\nTRAPEZIO: %0.3f\nQUADRADO: %0.3f\nRETANGULO: %0.3f" % (triangulo, circulo, trapezio, quadrado, retangulo))
+main()
