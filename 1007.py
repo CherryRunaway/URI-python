669e547a7887099aed8bd3eadaca3e8818b1b777
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-11 21:41:36 -0300

    Create 1007.py

---
+++ 1007.py
@@ -0,0 +1,10 @@
+def main():
+    a = input()
+    b = input()
+    c = input()
+    d = input()
+    
+    val = (int(a) * int(b)) - (int(c) * int(d))
+    
+    print("DIFERENCA = %s"%(val))
+main()
