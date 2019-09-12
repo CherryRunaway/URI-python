4dcc90e7b5f79ced7e3ddca61cd032dc25186c72
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-11 21:40:37 -0300

    Create 1006.py

---
+++ 1006.py
@@ -0,0 +1,12 @@
+def main():
+    n1 = input()
+    n2 = input()
+    n3 = input()
+    n1 = float(n1) * 0.2
+    n2 = float(n2) * 0.3
+    n3 = float(n3) * 0.5
+    
+    media = float(n1) + float(n2) + float(n3)
+    
+    print("MEDIA = %.1f"%(media))
+main()
