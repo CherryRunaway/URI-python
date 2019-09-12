d18e040e3948a8fa1eb253d1c16a2e36d9038f85
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-11 21:41:56 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,10 @@
+def main():
+    func = input()
+    horas = input()
+    val = input()
+    
+    salario = int(horas) * float(val)
+    
+    print("NUMBER = %s"%(func))
+    print("SALARY = U$ %.2f"%(salario))
+main()
