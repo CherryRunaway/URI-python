7b8a823acd279e7f3d2bab1d8cbab2b959056e8e
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-11 21:40:12 -0300

    Create 1005.py

---
+++ 1005.py
@@ -0,0 +1,10 @@
+def main():
+    n1 = input()
+    n2 = input()
+    n1 = float(n1) * 3.5
+    n2 = float(n2) * 7.5
+    
+    media = float(n1) + float(n2)
+    
+    print("MEDIA = %.5f"%(media/11))
+main()
