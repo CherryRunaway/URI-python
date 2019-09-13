a7235878e0d3560171443d5a25ac97b7745cba4b
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-13 11:54:33 -0300

    Create 2759.py

---
+++ 2759.py
@@ -0,0 +1,10 @@
+def main():
+    c1 = input()
+    c2 = input()
+    c3 = input()
+
+    print("A = %s, B = %s, C = %s" %(c1, c2, c3))
+    print("A = %s, B = %s, C = %s" %(c2, c3, c1))
+    print("A = %s, B = %s, C = %s" %(c3, c1, c2))
+    
+main()
