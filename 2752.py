cb85dea53f5bedc45f605ba7180dd003eea5c8ce
Isadora Rocha <isadora.rocha.de.almeida@gmail.com>
2019-09-14 14:19:06 -0300

    Create 2752.py

---
+++ 2752.py
@@ -0,0 +1,11 @@
+def main(): 
+    frase = "AMO FAZER EXERCICIO NO URI"
+    print("<%s>"%frase)
+    print("<%30s>"%frase)
+    print("<%.20s>"%frase)
+    print("<%-20s>"%frase)
+    print("<%-30s>"%frase)
+    print("<%.30s>"%frase)
+    print("<%30.20s>"%frase)
+    print("<%-30.20s>"%frase)
+main()
